c8d5a00b4b4c1b1424bb948c7527e32e650e8e19
jtcarlos13 <john.thomas.carlos0913@gmail.com>
2023-12-22 11:01:02 +0800

    Initial commit

---
+++ src/main/java/com/jtcarlos/springbootstudents/SpringbootStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.jtcarlos.springbootstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtcarlos/springbootstudents/SpringbootStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtcarlos.springbootstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
